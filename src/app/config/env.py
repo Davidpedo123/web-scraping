e958b42224db45968fbeb30827fe0f11323c4cca
Eduardo Tejada Moreta <etejada@inafocam.local>
2025-11-26 15:08:53 -0400

    add: Base para capturar precio y nombre de la pag2

--- src/app/config/env.py
+++ src/app/config/env.py
@@ -1,9 +1,13 @@
 from dotenv import load_dotenv
+from config.logger import logger
 import os
 
-load_dotenv('C:/Users/david/OneDrive - Instituto Tecnológico de Las Américas (ITLA)/Documentos/itla/c6/mineria de datos/Tarea 0304/env')
+load_dotenv('C:/Users/etejada/OneDrive - INAFOCAM/Documentos/Personal/web-scraping/.env')
 
 
 page1_url = os.environ.get('PAGE1_URL')
 page2_url = os.environ.get('PAGE2_URL')
 page3_url = os.environ.get('PAGE3_URL')
+
+
+logger.info(f"Loaded URLs from .env: {page1_url}, {page2_url}, {page3_url}")
--- src/app/service/consultHTTP.py
+++ src/app/service/consultHTTP.py
@@ -6,27 +6,44 @@ from config.logger import logger
 async def consult_page1():
     try:
         r = requests.get(page1_url, verify=False)
-        soup = BeautifulSoup(r.text, 'lxml')
+        soup = BeautifulSoup(r.content, "html.parser")
+        results = soup.find(id="ResultsContainer")
         return soup
     except Exception as e:
         logger.error(f"[{__name__}] Error consulting page 1: {e}")
         return None
 
+
 async def consult_page2():
     try:
         r = requests.get(page2_url, verify=False)
-        soup = BeautifulSoup(r.text, 'lxml')
-        return soup
+        soup = BeautifulSoup(r.content, "html.parser")
+
+        # Ajusta la clase para cada producto
+        products = soup.find_all("div", class_="product-card")  
+        products_all = []
+
+        for producto in products:
+            nombre = producto.find("div", class_="product-card__titles").get_text(strip=True)
+            precio = producto.find("div", attrs={"data-testid": "product-price"}).get_text(strip=True)
+            products_all.append({
+                "nombre": nombre,
+                "precio": precio
+            })
+
+        return products_all
+
     except Exception as e:
         logger.error(f"[{__name__}] Error consulting page 2: {e}")
         return None
-    
+
 async def consult_page3():
     try:
         r = requests.get(page3_url,verify=False)
-        soup = BeautifulSoup(r.text, 'lxml')
+        soup = BeautifulSoup(r.content, "html.parser")
+        results = soup.find(id="ResultsContainer")
         return soup
     except Exception as e:
         logger.error(f"[{__name__}] Error consulting page 3: {e}")
         return None
-  
+
